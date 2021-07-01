94f03899eef9a4e06b6eaeb9df1177c959eac835
NotII <46204250+NotII@users.noreply.github.com>
2021-07-01 12:49:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,55 +33,52 @@ async function setup() {
       return console.clear()
     }
   })
-}
-setup();
-
-
-
-const client = new presence.Client({ transport: "ipc" });
-client.on("ready", () => {
-  let data1 = new SelfReloadJSON("./data.json");
-  setInterval(() => {
-    processWindows.getProcesses((_err, processes) => {
-      const TIDAL = processes.filter((p) => p.processName.indexOf("TIDAL") >= 0);
-      for (const i of TIDAL) {
-        if (i.mainWindowTitle.length > 0) {
-          if (i.mainWindowTitle === "TIDAL") {
-            client.request("SET_ACTIVITY", {
-              pid: process.pid,
-              activity: {
-                details: `Paused`,
-                state: data1.song,
-                assets: {
-                  large_image: `tidal`,
-                  large_text: `Pasued: ${data1.song}`,
-                  small_image: `paused`,
+  const client = new presence.Client({ transport: "ipc" });
+  client.on("ready", () => {
+    let data1 = new SelfReloadJSON("./data.json");
+    setInterval(() => {
+      processWindows.getProcesses((_err, processes) => {
+        const TIDAL = processes.filter((p) => p.processName.indexOf("TIDAL") >= 0);
+        for (const i of TIDAL) {
+          if (i.mainWindowTitle.length > 0) {
+            if (i.mainWindowTitle === "TIDAL") {
+              client.request("SET_ACTIVITY", {
+                pid: process.pid,
+                activity: {
+                  details: `Paused`,
+                  state: data1.song,
+                  assets: {
+                    large_image: `tidal`,
+                    large_text: `Pasued: ${data1.song}`,
+                    small_image: `paused`,
+                  },
                 },
-              },
-            });
-          } else {
-            fs.writeFileSync(
-              "./data.json",
-              JSON.stringify({ song: i.mainWindowTitle })
-            );
-            client.request("SET_ACTIVITY", {
-              pid: process.pid,
-              activity: {
-                details: `Now Playing:`,
-                state: i.mainWindowTitle,
-                assets: {
-                  large_image: `tidal`,
-                  large_text: `Listening to ${i.mainWindowTitle}`,
-                  small_image: `playing`,
+              });
+            } else {
+              fs.writeFileSync(
+                "./data.json",
+                JSON.stringify({ song: i.mainWindowTitle })
+              );
+              client.request("SET_ACTIVITY", {
+                pid: process.pid,
+                activity: {
+                  details: `Now Playing:`,
+                  state: i.mainWindowTitle,
+                  assets: {
+                    large_image: `tidal`,
+                    large_text: `Listening to ${i.mainWindowTitle}`,
+                    small_image: `playing`,
+                  },
                 },
-              },
-            });
+              });
+            }
           }
         }
-      }
-    });
-  }, 1750);
-});
+      });
+    }, 1750);
+  });
 
-process.on("uncaughtException", (e) => {console.log(e.message)})
-client.login({ clientId: `825408902773342208` }).then(r => console.log(`Logged in as ${client.user.username}`)).catch(console.error())
+  process.on("uncaughtException", (e) => {console.log(e.message)})
+  client.login({ clientId: `825408902773342208` }).then(r => console.log(`Logged in as ${client.user.username}`)).catch(console.error())
+}
+setup();
